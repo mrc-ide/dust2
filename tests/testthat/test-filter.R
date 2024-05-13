90fad37d7a0fc461826f74c39da9170d6485538e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-05-13 14:53:49 +0100

    Add support for multiple groups at once

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -30,3 +30,37 @@ test_that("can run an unfilter", {
   expect_equal(dust2_cpu_sir_unfilter_run(ptr, pars1, FALSE), f(pars1))
   expect_equal(dust2_cpu_sir_unfilter_run(ptr, pars2, FALSE), f(pars2))
 })
+
+
+test_that("can run run unfilter on structured model", {
+  base <- list(beta = 0.1, gamma = 0.2, N = 1000, I0 = 10, exp_noise = 1e6)
+  n_groups <- 3
+  pars <- lapply(seq_len(n_groups),
+                 function(i) modifyList(base, list(beta = i * 0.1)))
+
+  time_start <- 0
+  time <- c(4, 8, 12, 16)
+  data <- lapply(1:4, function(i) {
+    lapply(seq_len(n_groups), function(j) list(incidence = 2 * (i - 1) + j))
+  })
+  dt <- 1
+
+  ## Manually compute likelihood:
+  f <- function(pars) {
+    obj <- dust2_cpu_sir_alloc(pars, time_start, dt, 1, n_groups, NULL, TRUE)
+    ptr <- obj[[1]]
+    dust2_cpu_sir_set_state_initial(ptr)
+    incidence <- matrix(0, n_groups, length(time))
+    time0 <- c(time_start, time)
+    for (i in seq_along(time)) {
+      dust2_cpu_sir_run_steps(ptr, round((time[i] - time0[i]) / dt))
+      incidence[, i] <- dust2_cpu_sir_state(ptr, FALSE)[5, , drop = TRUE]
+    }
+    observed <- matrix(unlist(data, use.names = FALSE), n_groups)
+    rowSums(dpois(observed, incidence + 1e-6, log = TRUE))
+  }
+
+  obj <- dust2_cpu_sir_unfilter_alloc(pars, time_start, time, dt, data, 3)
+  ptr <- obj[[1]]
+  expect_equal(dust2_cpu_sir_unfilter_run(ptr, NULL, FALSE), f(pars))
+})
