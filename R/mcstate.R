4ca1953a07c4cfe4b2a04693c402c9d371722532
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-06-11 17:37:13 +0100

    Update after mcstate changes

--- R/mcstate.R
+++ R/mcstate.R
@@ -4,41 +4,47 @@
 ##'
 ##' @param filter A `dust_filter` object, created from [dust_filter]
 ##'
-##' @param transformer A parameter transformer (unpacker?), which will
+##' @param packer A parameter packer (unpacker?), which will
 ##'   convert between an unstructured vector of parameters as used in
 ##'   an MCMC into the list of parameters that the dust system
 ##'   requires.
 ##'
+##' @param initial Optionally, a function to create initial conditions
+##'   from unpacked parameters.
+##'
 ##' @return A [mcstate::mcstate_model] object
-##' 
+##'
 ##' @export
-dust_filter_mcstate <- function(filter, transformer) {
+dust_filter_mcstate <- function(filter, packer, initial = NULL) {
   check_is_dust_filter(filter)
-  assert_is(transformer, "mcstate_transformer")
+  assert_is(packer, "mcstate_packer")
 
   ## We configure saving trajectories on creation I think, which then
   ## affects density.  Start without trajectories?  Realistically
-  ## people don't change this much afterwards?
+  ## people don't change this much afterwards?  This will be easiest
+  ## to think about once we sort out how models know about their variables.
 
-  ## TODO: transform needs to know about domain, I think, or at least
-  ## accept information on it.
+  ## TODO: Also accept information about domain to add here?  This
+  ## should be in terms of the packed vector.
+  domain <- NULL
 
-  is_stochastic <- !filter$deterministic
+  ## Supporting parameter groups requires some effort with packers,
+  ## movement there will come from mcstate, and probably we'll end up
+  ## subclassing the packer.
+  properties <- mcstate_model_properties(
+    is_stochastic = !filter$deterministic,
+    has_direct_sample = FALSE,
+    has_gradient = FALSE,
+    has_parameter_groups = FALSE)
 
   density <- function(x) {
-    pars <- transformer$transform(x)
-    ## TODO: initial state handling here, this requires some thought,
-    ## especially once we support arbitrary initial conditions, and
-    ## once these come from the parameters, and once we support
-    ## differentiation.  For now we require that the initialisation
-    ## can come from the model itself and later we will allow
-    ## overriding that - it's possible that is something that should
-    ## be done at the filter level tbh.
-    initial <- NULL
-    dust_filter_run(filter, pars, initial)
+    pars <- packer$unpack(x)
+    dust_filter_run(filter,
+                    pars,
+                    initial = if (is.null(initial)) NULL else initial(pars))
   }
 
-  if (is_stochastic) {
+  if (properties$is_stochastic) {
     get_rng_state <- function() {
       dust_filter_rng_state(filter)
     }
@@ -50,20 +56,13 @@ dust_filter_mcstate <- function(filter, transformer) {
     set_rng_state <- NULL
   }
 
-  properties <- mcstate_model_properties(
-    is_stochastic = is_stochastic,
-    has_direct_sample = FALSE,
-    has_gradient = FALSE,
-    ## We need to get this working with the transformation!
-    has_parameter_groups = FALSE)
-  
   mcstate2::mcstate_model(
     list(filter = filter,
          density = density,
          direct_sample = NULL,
          parameters = parameters,
          domain = domain,
-         set_rng_state = NULL,
-         get_rng_state = NULL),
+         set_rng_state = get_rng_state,
+         get_rng_state = set_rng_state),
     properties)
 }
