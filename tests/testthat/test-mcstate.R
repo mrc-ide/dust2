9ca4c1d03caf01834832169505d69348833f90b3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-07-25 18:50:31 +0100

    Expand testing

--- tests/testthat/test-mcstate.R
+++ tests/testthat/test-mcstate.R
@@ -56,3 +56,31 @@ test_that("can create deterministic model", {
   res <- mcstate2::mcstate_sample(m + prior, sampler, 10)
   expect_s3_class(res, "mcstate_samples")
 })
+
+
+test_that("can avoid errors by converting to impossible density", {
+  pars <- list(beta = 0.1, gamma = 0.2, N = 1000, I0 = 10, exp_noise = 1e6)
+
+  time_start <- 0
+  time <- c(4, 8, 12, 16)
+  data <- lapply(1:4, function(i) list(incidence = i))
+  dt <- 1
+  n_particles <- 100
+  seed <- 42
+
+  obj <- dust_unfilter_create(sir(), time_start, time, data)
+  packer <- mcstate2::mcstate_packer(
+    c("beta", "gamma"),
+    fixed = list(N = 1000, I0 = 10, exp_noise = 1e6))
+  prior <- mcstate2::mcstate_dsl({
+    beta ~ Exponential(mean = 0.5)
+    gamma ~ Exponential(mean = 0.5)
+  })
+
+  m1 <- dust_filter_mcstate(obj, packer)
+  m2 <- dust_filter_mcstate(obj, packer, failure_is_impossible = TRUE)
+
+  expect_error(m1$density(c(-1, -1)),
+               "Invalid call to binomial")
+  expect_equal(m2$density(c(-1, -1)), -Inf)
+})
