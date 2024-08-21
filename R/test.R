b197c360d8448545942870c699f4b1bd80d21243
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-08-21 07:41:12 +0100

    Update test helper

--- R/test.R
+++ R/test.R
@@ -1,4 +1,6 @@
 test_history <- function(time, state, order = NULL, index_group = NULL,
-                         reorder = FALSE) {
-  test_history_(time, state, order, index_group, reorder)
+                         index_particle = NULL, reorder = FALSE) {
+  ## TODO: move index_particle ahead of index_group everywhere, once
+  ## we're stable.
+  test_history_(time, state, order, index_group, index_particle, reorder)
 }
