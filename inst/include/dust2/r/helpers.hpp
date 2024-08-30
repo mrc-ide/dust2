b9efde347a33b98cecc6b0d6885672136590d5b6
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-08-30 16:47:13 +0100

    Fix index error when reading shared

--- inst/include/dust2/r/helpers.hpp
+++ inst/include/dust2/r/helpers.hpp
@@ -307,7 +307,7 @@ std::vector<typename T::data_type> check_data(cpp11::list r_data,
     check_length(r_data_i, n_groups, "data[i]");
     for (size_t j = 0; j < n_groups; ++j) {
       auto r_data_ij = cpp11::as_cpp<cpp11::list>(r_data_i[j]);
-      data.push_back(T::build_data(r_data_ij, shared[i]));
+      data.push_back(T::build_data(r_data_ij, shared[j]));
     }
   }
 
