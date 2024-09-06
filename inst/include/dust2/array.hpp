565756d3532cde2248a90b39683ea8b0e8896332
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-06 17:46:01 +0100

    Change initialisation

--- inst/include/dust2/array.hpp
+++ inst/include/dust2/array.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
-#include <functional>
-#include <numeric>
 #include <stdexcept>
 #include <sstream>
 #include <vector>
@@ -13,24 +12,20 @@ namespace array {
 template <size_t rank_>
 struct dimensions {
   size_t rank;
-  std::array<size_t, rank_> dim;
   size_t size;
+  std::array<size_t, rank_> dim;
   std::array<size_t, rank_> mult;
   // One disadvantage here is we can't optimise away the case where
   // arrays have literal size, we might want to add that in as an
   // optimisation later?
   dimensions(std::initializer_list<size_t> dim_) :
     rank(rank_),
-    dim(dim_),
-    size(std::accumulate(dim.begin(), dim.end(), 1, std::multiplies<size_t>())) {
-    // TODO: I think we can fail to compile here if rank does not
-    // match dim_.size()?
-    //
-    // TODO: I think we can drop 2 of these, so only bother doing
-    // this in the case where we have 3 dimensions or more and store 2
-    // fewer values (with slightly more complex calculations).
-    for (size_t i = 0; i < rank; ++i) {
-      mult[i] = i == 0 ? 1 : dim[i - 1] * mult[i - 1];
+    size(1) {
+    auto iter = dim_.begin();
+    for (size_t i = 0; i < rank; ++i, ++iter) {
+      dim[i] = *iter;
+      mult[i] = size;
+      size *= dim[i];
     }
   }
 };
@@ -65,28 +60,27 @@ inline size_t index2(size_t at_r, size_t at_c, size_t size_r, size_t size_c,
   return at_r + at_c * size_r;
 }
 
+template <size_t rank>
 size_t index(std::initializer_list<size_t> at,
-             const std::vector<size_t>& dim,
-             const std::vector<size_t>& mult,
+             const dimensions<rank> dim,
              const char *name,
              const char *context) {
-  const auto rank = dim.size();
   size_t ret = 0;
   auto it = at.begin();
   for (size_t i = 0; i < rank; ++i, ++it) {
     const auto v = *it;
-    if (v >= dim[i]) {
+    if (v >= dim.dim[i]) {
       std::stringstream msg;
       msg << "Tried to access element " << v + 1 << " from dimension " <<
         i + 1 << " of array " << name <<
         ", which is out of bounds (extent of this dimension is " <<
-        dim[i] << ").";
+        dim.dim[i] << ").";
       if (context != nullptr && *context != 0) {
         msg << " " << context;
       }
       throw std::runtime_error(msg.str());
     }
-    ret += mult[i] * v;
+    ret += dim.mult[i] * v;
   }
   return ret;
 }
