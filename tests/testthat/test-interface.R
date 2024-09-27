72e9f564ead19149e3605ef2cf08a846ea5e1ba4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-27 07:57:01 +0100

    Tidy up tests

--- tests/testthat/test-interface.R
+++ tests/testthat/test-interface.R
@@ -193,34 +193,3 @@ test_that("format dimensions as a string", {
                            n_groups = 1)),
     "single particle")
 })
-
-
-test_that("can unpack state from systems with a single particle", {
-  sys <- dust_system_create(sir(), list(), n_particles = 1)
-  dust_system_set_state_initial(sys)
-  s <- dust_system_state(sys)
-  s2 <- dust_unpack_state(sys, s)
-  expect_equal(s2, sys$packer_state$unpack(s))
-  expect_equal(s2, list(S = 990, I = 10, R = 0, cases_cumul = 0, cases_inc = 0))
-})
-
-
-test_that("can unpack state from systems with several particles", {
-  sys <- dust_system_create(sir(), list(), n_particles = 10)
-  dust_system_set_state_initial(sys)
-  s <- dust_system_state(sys)
-  s2 <- dust_unpack_state(sys, s)
-  expect_equal(s2, sys$packer_state$unpack(s))
-  expect_equal(lengths(s2, FALSE), rep(10, 5))
-})
-
-
-test_that("Can unpack history", {
-  sys <- dust_system_create(sir(), list(), n_particles = 10)
-  dust_system_set_state_initial(sys)
-  s <- dust_system_simulate(sys, 0:20)
-  ## I think that this is a bug in monty; we've preserved a state
-  ## dimension here that we should know that we should be dropping,
-  ## because we have things down as integer(0)
-  s2 <- dust_unpack_state(sys, s)
-})
