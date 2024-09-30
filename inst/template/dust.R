d78c39e4c5b70ea35fab3830a66366706b8336d0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-30 20:09:50 +0100

    Fix environment on generator load

--- inst/template/dust.R
+++ inst/template/dust.R
@@ -1,3 +1,8 @@
 {{name}} <- function() {
-  dust2::dust_system_generator("{{name}}", "{{time_type_property}}", {{default_dt}})
+  if (isNamespaceLoaded("{{package}}")) {
+    env <- asNamespace("{{package}}")
+  } else {
+    env <- dust2:::load_temporary_package("{{{{path_pkg}}}}", "{{package}}", FALSE)
+  }
+  dust2::dust_system_generator("{{name}}", "{{time_type_property}}", {{default_dt}}, env)
 }
