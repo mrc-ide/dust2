b71dbbbecbafbcdc281c641c4b46ad02c5aff3fb
Wes Hinsley <w.hinsley@imperial.ac.uk>
2024-10-02 16:19:09 +0100

    Fix local win test failure

--- tests/testthat/test-zzz-compile.R
+++ tests/testthat/test-zzz-compile.R
@@ -43,6 +43,6 @@ test_that("can compile into a stable directory", {
 
   log_txt <- readLines(log)
   expect_match(log_txt, "'src/dust.cpp' is up to date", all = FALSE)
-  expect_match(log_txt, "Loading mysir2", all = FALSE)
+  expect_match(log_txt, "Loading(.+)mysir2", all = FALSE)
   expect_false(any(grepl("compiling", log_txt, ignore.case = TRUE)))
 })
