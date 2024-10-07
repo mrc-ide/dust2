9394c32fe74715161a16d6c1f03f75cb65c47720
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-10-07 11:10:15 +0100

    Simplify example

--- tests/testthat/examples/debug.cpp
+++ tests/testthat/examples/debug.cpp
@@ -3,8 +3,6 @@
 
 // [[dust2::class(sirdebug)]]
 // [[dust2::time_type(discrete)]]
-// [[dust2::has_compare()]]
-// [[dust2::has_adjoint()]]
 // [[dust2::parameter(I0)]]
 // [[dust2::parameter(N)]]
 // [[dust2::parameter(beta)]]
@@ -34,6 +32,10 @@ public:
 
   static dust2::packing packing_state(const shared_state& shared) {
     return dust2::packing{{"S", {}}, {"I", {}}, {"R", {}}, {"cases_cumul", {}}, {"cases_inc", {}}};
+  }
+
+  static dust2::packing packing_gradient(const shared_state& shared) {
+    return dust2::packing{};
   }
 
   static void initial(real_type time,
@@ -71,8 +73,6 @@ public:
     state_next[3] = cases_cumul + n_SI;
     state_next[4] = cases_inc + n_SI;
 
-    Rprintf("at time %f\n", time);
-
     if (time > 2 && time < 5) {
       auto env = dust2::r::debug::create_env();
       dust2::r::debug::save(time, "time", env);
@@ -108,105 +108,5 @@ public:
 
   static auto zero_every(const shared_state& shared) {
     return dust2::zero_every_type<real_type>{{1, {4}}}; // zero[1] = {4};
-  }
-
-  static data_type build_data(cpp11::list r_data, const shared_state& shared) {
-    auto data = static_cast<cpp11::list>(r_data);
-    auto incidence = dust2::r::read_real(data, "incidence", NA_REAL);
-    return data_type{incidence};
-  }
-
-  static real_type compare_data(const real_type time,
-                                const real_type * state,
-                                const data_type& data,
-                                const shared_state& shared,
-                                internal_state& internal,
-                                rng_state_type& rng_state) {
-    const auto incidence_observed = data.incidence;
-    if (std::isnan(data.incidence)) {
-      return 0;
-    }
-    const auto noise =
-      monty::random::exponential_rate(rng_state, shared.exp_noise);
-    const auto incidence_modelled = state[4];
-    const auto lambda = incidence_modelled + noise;
-    return monty::density::poisson(incidence_observed, lambda, true);
-  }
-
-  static dust2::packing packing_gradient(const shared_state& shared) {
-    return dust2::packing{{"beta", {}}, {"gamma", {}}, {"I0", {}}};
-  }
-
-  static void adjoint_initial(real_type time,
-                              const real_type * state,
-                              const real_type * adjoint,
-                              const shared_state& shared,
-                              internal_state& internal,
-                              real_type * adjoint_next) {
-    const real_type adj_I = adjoint[1];
-    adjoint_next[0] = adjoint[0];
-    adjoint_next[1] = adjoint[1];
-    adjoint_next[2] = adjoint[2];
-    adjoint_next[3] = adjoint[3];
-    adjoint_next[4] = adjoint[4];
-    adjoint_next[5] = adjoint[5];
-    adjoint_next[6] = adjoint[6];
-    adjoint_next[7] = adjoint[7] + adj_I;
-  }
-
-  static void adjoint_update(real_type time,
-                             real_type dt,
-                             const real_type * state,
-                             const real_type * adjoint,
-                             const shared_state& shared,
-                             internal_state& internal,
-                             real_type * adjoint_next) {
-    const auto S = state[0];
-    const auto I = state[1];
-
-    const auto p_SI = 1 - monty::math::exp(-shared.beta * I / shared.N * dt);
-    const auto p_IR = 1 - monty::math::exp(-shared.gamma * dt);
-
-    const auto adj_S = adjoint[0];
-    const auto adj_I = adjoint[1];
-    const auto adj_R = adjoint[2];
-    const auto adj_cases_cumul = adjoint[3];
-    const auto adj_cases_inc = adjoint[4];
-    const auto adj_beta = adjoint[5];
-    const auto adj_gamma = adjoint[6];
-    const auto adj_I0 = adjoint[7];
-
-    const auto adj_lambda = 0;
-    const auto adj_n_IR = -adj_I + adj_R;
-    const auto adj_n_SI = adj_cases_inc + adj_cases_cumul + adj_I + -adj_S;
-    const auto adj_p_IR = adj_n_IR * I;
-    const auto adj_p_SI = adj_n_SI * S;
-
-    adjoint_next[0] = adj_n_SI * p_SI + adj_S;
-    adjoint_next[1] = adj_n_IR * p_IR + adj_p_SI * (shared.beta * dt) * monty::math::exp(-shared.beta * I * dt / shared.N) / shared.N + adj_I;
-    adjoint_next[2] = adj_R;
-    adjoint_next[3] = adj_cases_cumul;
-    adjoint_next[4] = adj_lambda + adj_cases_inc * (dust2::tools::is_evenly_divisible_by(time, static_cast<real_type>(1)) ? 0 : 1);
-    adjoint_next[5] = adj_beta + adj_p_SI * (I * dt) * monty::math::exp(-shared.beta * I * dt / shared.N) / shared.N;
-    adjoint_next[6] = adj_gamma + adj_p_IR * monty::math::exp(-shared.gamma);
-    adjoint_next[7] = adj_I0;
-  }
-
-  static void adjoint_compare_data(real_type time,
-                                   const real_type * state,
-                                   const real_type * adjoint,
-                                   const data_type& data,
-                                   const shared_state& shared,
-                                   internal_state& internal,
-                                   real_type * adjoint_next) {
-    const real_type cases_inc = state[4];
-    adjoint_next[0] = adjoint[0];
-    adjoint_next[1] = adjoint[1];
-    adjoint_next[2] = adjoint[2];
-    adjoint_next[3] = adjoint[3];
-    adjoint_next[4] = adjoint[4] + data.incidence / cases_inc - 1;
-    adjoint_next[5] = adjoint[5];
-    adjoint_next[6] = adjoint[6];
-    adjoint_next[7] = adjoint[7];
   }
 };
