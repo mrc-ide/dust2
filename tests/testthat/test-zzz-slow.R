9562ada038113c613614a94c45b5a958e8110a52
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-10-14 16:34:54 +0100

    Fix crash using internal
    Adds integration test too

--- tests/testthat/test-zzz-slow.R
+++ tests/testthat/test-zzz-slow.R
@@ -22,3 +22,18 @@ test_that("can use two zero_every", {
   expect_equal(y[1, ], rep(1:2, length.out = 20))
   expect_equal(y[2, ], rep(1:5, length.out = 20))
 })
+
+
+test_that("can run a model with internal storage", {
+  skip_for_compilation()
+  gen <- dust_compile("examples/internal.cpp", quiet = TRUE, debug = TRUE)
+
+  len <- 5
+  set.seed(1)
+  sys <- dust_system_create(gen(), list(sd = 1, len = len), 1)
+  seed <- dust_system_rng_state(sys)
+  y <- drop(dust_system_simulate(sys, 1:20))
+
+  r <- monty::monty_rng$new(seed = seed)$normal(20 * len, 0, 1)
+  expect_equal(y, cumsum(colMeans(matrix(r, len, 20))))
+})
