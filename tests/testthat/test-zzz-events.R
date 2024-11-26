4d3e3c8056aa849a8e666093cf3f5db23956e956
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-11-26 10:59:29 +0000

    Fix test

--- tests/testthat/test-zzz-events.R
+++ tests/testthat/test-zzz-events.R
@@ -1,19 +1,30 @@
 test_that("can run system with roots and events", {
   gen <- dust_compile("examples/event.cpp", quiet = TRUE, debug = TRUE)
 
-  sys <- dust_system_create(gen)
+  control <- dust_ode_control(debug_record_step_times = TRUE, save_history = TRUE)
+  sys <- dust_system_create(gen, ode_control = control)
   dust_system_set_state_initial(sys)
-  t <- seq(0, 6, length.out = 500)
+
+  ## Use relatively few points for output here as this exacerbates
+  ## problems, even though the solution looks silly.
+  t <- seq(0, 6, length.out = 60)
   y <- dust_system_simulate(sys, t)
+  cmp <- example_bounce_analytic(t)
 
-  info <- dust_system_internals(sys)
-  expect_equal(nrow(info$events[[1]]), 3)
+  info <- dust_system_internals(sys, include_history = TRUE)
 
-  ## This is realy not great, but at this point I don't know who is
-  ## worse.  Qualitatively we're about right though and I need to go
-  ## through and compare with an analytic solution as here we've got
-  ## two different approximations and a nonlinear system that is
-  ## acumulating error.
-  cmp <- example_bounce(t)
-  expect_equal(y[1, ], cmp[, 2], tolerance = 1e-2)
+  ## Find all roots:
+  r <- info$events[[1]]
+  expect_equal(nrow(r, 3))
+  expect_equal(r$time, cmp$roots, tolerance = 1e-6)
+  expect_equal(r$index, rep(0L, 3))
+  expect_equal(r$sign, rep(-1, 3))
+
+  ## Stop at all roots:
+  h <- info$history[[1]]
+  expect_true(all(r$time %in% h$t0))
+  expect_true(all(r$time %in% h$t1))
+
+  ## Overall solution:
+  expect_equal(y[1, ], cmp$y, tolerance = 1e-6)
 })
